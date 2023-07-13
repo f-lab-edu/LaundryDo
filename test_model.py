0b7b18e8e74d4d954eb0d63055febbef2886b523
eunsung <eunsung.shin@gmail.com>
2023-07-13 23:04:52 +0900

    test divide order into laundrybag

--- test_model.py
+++ test_model.py
@@ -1,6 +1,25 @@
 import pytest
-from model import Clothes, Order, LaundryLabel, User
+
+from uuid import uuid4
+from model import Clothes, Order, LaundryLabel, LaundryBag, User
+
+from typing import List
+
 from datetime import datetime
+
+time_now = datetime.now()
+
+def divide_order(order : Order)  -> List[LaundryBag] :
+    laundrylabeldict = {}
+    for clothes in order :
+        if clothes.label in laundrylabeldict : 
+            laundrylabeldict[clothes.label].append(clothes)
+        else :
+            laundrylabeldict[clothes.label] = [clothes]
+
+    return laundrylabeldict
+
+
 
 
 
@@ -18,11 +37,21 @@ def test_user_cancel_order(new_user, new_order ) :
 
     assert new_user.orderlist == []
     
+def test_clothes_in_an_order_has_all_same_order_id(new_order) :
+
+    assert len(set(order.orderid for order in new_order)) == 1
 
 
-def test_order_sort_by_laundrybags(new_order) :
-    # new_order.sortbyLaundryLabel()
-    pass
+def test_order_sort_by_laundrybags() :
+    clothes1 = Clothes(clothesid = str(uuid4())[:4], label = LaundryLabel.WASH, volume = 0.3)
+    clothes2 = Clothes(clothesid = str(uuid4())[:4], label = LaundryLabel.DRY, volume = 0.3)
+    clothes3 = Clothes(clothesid = str(uuid4())[:4], label = LaundryLabel.DRY, volume = 0.3)
+    clothes4 = Clothes(clothesid = str(uuid4())[:4], label = LaundryLabel.WASH, volume = 0.3)
+    order = Order('order1', received_at = time_now, clothes_list = [clothes1, clothes2, clothes3, clothes4])
+
+    laundrylabeldict = divide_order(order)
+
+    assert len(laundrylabeldict.keys()) == 2
     
 
 
