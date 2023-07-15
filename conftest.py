fbd78f43b05073bad57a96848262ec1a0a0d3498
eunsungshin <eunsung.shin@gmail.com>
2023-07-15 16:05:19 +0900

    conftest TODO : clean duplicates

--- conftest.py
+++ conftest.py
@@ -1,19 +1,25 @@
 from uuid import uuid4
 import random
 
-from model import Clothes, ClothesState, LaundryLabel, Order, User
+from model import Clothes, ClothesState, LaundryBag, LaundryLabel, Order, User
 from datetime import datetime
 import pytest
 
 time_now = datetime(2023, 7, 12, 20, 48, 13)
 
+def new_clothes(label = None, volume = None, status = None) :
 
-def new_clothes() :
     id = str(uuid4())[:4]
-    label = random.choice([LaundryLabel.WASH, LaundryLabel.DRY, LaundryLabel.HAND])
-    volume = random.randint(5, 15)
-    status = random.choice([ClothesState.PREPARING, ClothesState.CANCELLED, ClothesState.DIVIDED, ClothesState.PROCESSING, ClothesState.DONE])
+    if label is None :
+        label = random.choice([LaundryLabel.WASH, LaundryLabel.DRY, LaundryLabel.HAND])
+
+    if volume is None : 
+        volume = random.randint(5, 15)
+    if status is None :
+        status = random.choice([ClothesState.PREPARING, ClothesState.CANCELLED, ClothesState.DIVIDED, ClothesState.PROCESSING, ClothesState.DONE])
     return Clothes(id = id, label = label, volume = volume, status = status)
+            
+
 
 @pytest.fixture
 def new_user() :
@@ -25,3 +31,4 @@ def new_order() :
     order = Order('order1', received_at = time_now, clothes_list = [new_clothes() for _ in range(10)])
 
     return order
+
