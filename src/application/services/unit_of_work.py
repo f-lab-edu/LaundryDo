3c3e1fe7f8a4797ceb1954e6abdbc8fe4901624e
eunsungshin <eunsung.shin@gmail.com>
2023-07-24 16:19:00 +0900

    add uow pattern

---
+++ src/application/services/unit_of_work.py
@@ -0,0 +1,69 @@
+import abc
+from abc import abstractmethod
+
+from src.domain.repository import  (
+    UserRepository,
+    OrderRepository,
+    ClothesRepository,
+    LaundryBagRepository,
+    MachineRepository
+
+)
+from src.infrastructure.db.sqlalchemy.setup import session
+from src.infrastructure.db.sqlalchemy.repository import (
+    SqlAlchemyClothesRepository,
+    SqlAlchemyLaundryBagRepository,
+    SqlAlchemyMachineRepository,
+    SqlAlchemyOrderRepository,
+    SqlAlchemyUserRepository
+)
+
+class AbstractUnitOfWork(abc.ABC):
+    users : UserRepository
+    orders : OrderRepository
+    clothes : ClothesRepository
+    laundrybags : LaundryBagRepository
+    machines : MachineRepository
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    users : UserRepository
+    orders : OrderRepository
+    clothes : ClothesRepository
+    laundrybags : LaundryBagRepository
+    machines : MachineRepository
+
+
+    def __init__(self, session_factory = session):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory() 
+        self.users = SqlAlchemyUserRepository(self.session)
+        self.orders = SqlAlchemyOrderRepository(self.session)
+        self.clothes = SqlAlchemyClothesRepository(self.session)
+        self.laundrybags = SqlAlchemyLaundryBagRepository(self.session)
+        self.machines = SqlAlchemyMachineRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
