bdca0f89df71d8c0b081c5f0519fcb8f0c6f6081
eunsungshin <eunsung.shin@gmail.com>
2023-07-24 16:19:31 +0900

    add service layer

---
+++ tests/unit/test_services.py
@@ -0,0 +1,22 @@
+from src.domain.services import (
+    request_order,
+    cancel_order,
+    distribute_order,
+    put_in_laundrybag,
+    reclaim_clothes_into_order,
+    get_clothes_in_process,
+    allocate,
+    ship
+)
+
+
+class FakeSession:
+    committed = False
+
+    def commit(self):
+        self.committed = True
+
+
+
+def test_request_order(order_factory) :
+    pass
